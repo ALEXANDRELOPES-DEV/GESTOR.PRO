783365a59c17fb66bf005224df3eacb7825f5802
ALEXANDRELOPES-DEV <alexandrelopes.info@gmail.com>
2025-01-26 09:49:33 -0300

    up

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
 // Lista de usuários permitidos e suas senhas
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyAC9RvILVr4_kuMhFcJ54JIv82U1AAPe0o",
+    authDomain: "aumozarifado.firebaseapp.com",
+    databaseURL: "https://aumozarifado-default-rtdb.firebaseio.com",
+    projectId: "aumozarifado",
+    storageBucket: "aumozarifado.firebasestorage.app",
+    messagingSenderId: "109255593715",
+    appId: "1:109255593715:web:424f9e55ef464cd1857186",
+    measurementId: "G-VC20C3LCZW"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
+
 const users = {
     "alex": "1985", // Atualizado
     "magno": "2014",
@@ -151,25 +175,3 @@ window.addEventListener("load", () => {
     document.getElementById("logoutButton").disabled = true; // Desabilita o botão Deslogar inicialmente
     document.getElementById("saveButton").disabled = true; // Desabilita o botão Salvar Alterações inicialmente
 });
-const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    databaseURL: "YOUR_DATABASE_URL",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
-  };
-  firebase.initializeApp(firebaseConfig);
-  const database = firebase.database();
-// Escrever dados
-firebase.database().ref('caminho/para/dados').set({
-    chave: "valor"
-  });
-  
-  // Ler dados
-  firebase.database().ref('caminho/para/dados').on('value', (snapshot) => {
-    const data = snapshot.val();
-    console.log(data);
-  });
-    
